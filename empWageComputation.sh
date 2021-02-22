b2f2f68611ec6c4fe331b4adee5f753b2653f6d3
sanjana k <sanjukarthijan27@gmail.com>
2021-02-22 19:04:40 +0530

    added empWageComputation.sh

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+attendence=$(( RANDOM % 2 ))
+if(( attendence == 0 ))
+then
+	 echo "employee is present"
+else
+	 echo "employee is absent"
+fi
 
 
 echo "welcome to employee wage computation"
